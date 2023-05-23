1ee1a6a49a44c67ec50acff621575c056a906686
Dev Chauhan <devccode8@gmail.com>
2023-05-23 21:17:38 +0530

    some update

---
+++ assets/js/timer.js
@@ -0,0 +1,25 @@
+// Timer
+
+var time="2 june 2023 11:00:00";
+const countDate = new Date(time).getTime();
+var x = setInterval(function () {
+    let now = new Date().getTime();
+    let distance = countDate - now;
+
+    var d = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hrs = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var min = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var sec = Math.floor((distance % (1000 * 60)) / (1000));
+
+    document.getElementById("days").innerHTML= d + "d ";
+    document.getElementById("hours").innerHTML= hrs + "h  ";
+    document.getElementById("minutes").innerHTML= min + "m ";
+    document.getElementById("seconds").innerHTML= sec + "s ";
+
+    // if(distance<0){
+    //     x= setInterval(0);
+    //     try{document.getElementById("timer").innerHTML= "No event is available right now.";
+    // }catch(e){}
+    // }
+})
+
